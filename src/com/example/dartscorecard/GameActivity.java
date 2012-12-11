47aed0235494d6ee1046d6548f2e8606bea9b688
Brennan Payne <brennanpayne2@gmail.com>
2012-12-11 15:34:35 -0600

    Built the beginning of the game for selecting the number of players

--- src/com/example/dartscorecard/GameActivity.java
+++ src/com/example/dartscorecard/GameActivity.java
@@ -2,16 +2,22 @@ package com.example.dartscorecard;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RadioGroup;
 
 public class GameActivity extends Activity {
-
+	private static final String TAG = "GameActivity"; 
+	Button startButton;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_game);
+		startButton = (Button) findViewById(R.id.startButton);
+		startButton.setOnClickListener(startHandler);
 	}
 
 	@Override
@@ -21,22 +27,22 @@ public class GameActivity extends Activity {
 		return true;
 	}
 	
+	// This is triggered after the number of users has been selected
 	View.OnClickListener startHandler = new View.OnClickListener() {
 		
 		@Override
 		public void onClick(View v) {
 			RadioGroup g = (RadioGroup) findViewById(R.id.rPlayerGroup);
 			int numPlayers = -1;
-			
 			switch(g.getCheckedRadioButtonId()){
-				case 1: numPlayers = 2;
+				case R.id.twoPlayer: numPlayers = 2;
 						break;
-				case 2: numPlayers = 3;
+				case R.id.threePlayer: numPlayers = 3;
 						break;
-				case 3: numPlayers = 4;
+				case R.id.fourPlayer: numPlayers = 4;
 						break;
 			}
-			
+			Log.i(TAG, "numPlayers: " + numPlayers);
 		}
 	};
 }
