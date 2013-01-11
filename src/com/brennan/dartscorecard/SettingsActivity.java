8cefa60987ad49a1f28273a30062b8e18597e7dc
Brennan Payne <brennanpayne2@gmail.com>
2013-01-10 22:31:56 -0600

    Fully implemented Hammer settings page with the ability to add and subtract players and their names

--- src/com/brennan/dartscorecard/SettingsActivity.java
+++ src/com/brennan/dartscorecard/SettingsActivity.java
@@ -7,27 +7,56 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
-import android.widget.RadioGroup;
-
-import com.brennan.dartscorecard.R;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
 import com.brennan.gamelogic.HammerGame;
 import com.brennan.gamelogic.Player;
 
 public class SettingsActivity extends Activity {
+	private RelativeLayout mLayout;
+	private EditText[] mPlayerNames;
+	private Button mAddMorePlayersButton, mLessPlayersButton, mStartButton;
+	private int numPlayers;
+	
 	private static final String TAG = "GameActivity"; 
-	Button startButton;
 	private String gameType;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		//Retrieve extras
 		Bundle extras = getIntent().getExtras();
 		if(extras != null){
 			gameType = extras.getString("gameType");
 		}
+		numPlayers = 1;
+		
+		//Set Layout
 		setContentView(R.layout.activity_settings);
-		startButton = (Button) findViewById(R.id.startButton);
-		startButton.setOnClickListener(startHandler);
+		mLayout = (RelativeLayout) findViewById(R.id.relativeLayoutSettings);
+		
+		//The edit texts of the players name
+		//There is probably a better way to do this than creating and hiding all edit texts
+		mPlayerNames = new EditText[4]; 
+		mPlayerNames[0] = (EditText) findViewById(R.id.playerOneInput);
+		mPlayerNames[1] = (EditText) findViewById(R.id.playerTwoInput);
+		mPlayerNames[1].setVisibility(View.GONE);
+		mPlayerNames[2] = (EditText) findViewById(R.id.playerThreeInput);
+		mPlayerNames[2].setVisibility(View.GONE);
+		mPlayerNames[3] = (EditText) findViewById(R.id.playerFourInput);
+		mPlayerNames[3].setVisibility(View.GONE);
+
+		//Set Buttons
+		mStartButton = (Button) findViewById(R.id.startButton);
+		mStartButton.setOnClickListener(startHandler);			
+		
+		mAddMorePlayersButton = (Button) findViewById(R.id.addMorePlayersButton);
+		mAddMorePlayersButton.setOnClickListener(addMorePlayersHandler);
+		
+		mLessPlayersButton = (Button) findViewById(R.id.lessPlayersButton);
+		mLessPlayersButton.setOnClickListener(lessPlayersHandler);
+		mLessPlayersButton.setVisibility(View.INVISIBLE);
 	}
 
 	@Override
@@ -37,6 +66,38 @@ public class SettingsActivity extends Activity {
 		return true;
 	}
 	
+	/*
+	 * Shows the next edit text for the next players
+	 */
+	View.OnClickListener addMorePlayersHandler = new View.OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			showNextPlayerField();
+			numPlayers++;
+			if (numPlayers >= 4){
+				mAddMorePlayersButton.setVisibility(View.INVISIBLE);
+			}
+			mLessPlayersButton.setVisibility(View.VISIBLE);
+		}
+	};
+	
+	/*
+	 * Removes the most recent edit text for players
+	 */
+	View.OnClickListener lessPlayersHandler = new View.OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			numPlayers--;
+			removePreviousPlayerField();
+			if (numPlayers <= 1){
+				mLessPlayersButton.setVisibility(View.INVISIBLE);
+			}
+			mAddMorePlayersButton.setVisibility(View.VISIBLE);		
+		}
+	};
+	
 	 /* 
 	 * This is triggered after the number of users has been selected
 	 * On return it starts this same intent with new params (number of player)
@@ -45,17 +106,6 @@ public class SettingsActivity extends Activity {
 		
 		@Override
 		public void onClick(View v) {
-						
-			RadioGroup g = (RadioGroup) findViewById(R.id.rPlayerGroup);
-			int numPlayers = -1;
-			switch(g.getCheckedRadioButtonId()){
-				case R.id.twoPlayer: numPlayers = 2;
-						break;
-				case R.id.threePlayer: numPlayers = 3;
-						break;
-				case R.id.fourPlayer: numPlayers = 4;
-						break;
-			}
 			
 			Log.i(TAG, "Creating " + numPlayers + " players");
 			
@@ -68,8 +118,15 @@ public class SettingsActivity extends Activity {
 			Log.i(TAG, "Created players and added them to game");
 			Intent intent = new Intent(getBaseContext(), HammerGameActivity.class);
 			intent.putExtra("numPlayers", numPlayers);
-			intent.putExtra("gameType", gameType);
+			intent.putExtra("game", game);
 			startActivity(intent);
 		}
 	};
+
+	private void removePreviousPlayerField(){
+		mPlayerNames[numPlayers].setVisibility(View.GONE);
+	}
+	private void showNextPlayerField(){
+		mPlayerNames[numPlayers].setVisibility(View.VISIBLE);
+	}
 }
