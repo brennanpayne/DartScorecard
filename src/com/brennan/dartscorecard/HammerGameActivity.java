1238316e8e757ebe2e767cde4763ea444bf245fa
Brennan Payne <brennanpayne2@gmail.com>
2013-01-20 17:33:35 -0600

    Next Turn is now working with multiple players

--- src/com/brennan/dartscorecard/HammerGameActivity.java
+++ src/com/brennan/dartscorecard/HammerGameActivity.java
@@ -6,12 +6,10 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.graphics.Color;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.Button;
-import android.widget.FrameLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -26,6 +24,8 @@ public class HammerGameActivity extends Activity {
 	private TextView round_mark, dart2_text, dart3_text;
 	private RelativeLayout mRelativeLayout;
 	ArrayList<Player> players;
+	private ArrayList<String> playerNames;
+	private int numTurns;
 	private Button nextTurnButton, prevTurnButton;
 	HammerGame game;
 
@@ -40,89 +40,92 @@ public class HammerGameActivity extends Activity {
 		}
 
 		Log.v(TAG,"Recived players in Hammer Game");
+		playerNames = new ArrayList<String>();
+		
+		for(int i = 0; i < players.size(); i++)
+			playerNames.add(players.get(i).getName());
+		
 
 		game = new HammerGame();
 		game.addPlayers(players);
-		//game.setCurrentRound(game.getMarks().get(0));
+		numTurns = 0;
 		setContentView(R.layout.activity_hammer_game);
+
 		
 		mRelativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
 		round_mark = (TextView) findViewById(R.id.round_mark);
 		dart2_text = (TextView) findViewById(R.id.dart2_text);
 		dart3_text = (TextView) findViewById(R.id.dart3_text);
-		
+
 		nextTurnButton = (Button) findViewById(R.id.next_turn_button);
 		nextTurnButton.setOnClickListener(nextTurnHandler);
-		
+
 		prevTurnButton = (Button) findViewById(R.id.prev_turn_button);
 		prevTurnButton.setEnabled(false);
 		prevTurnButton.setOnClickListener(prevTurnHandler);
 		
-		//		for(int i = 0; i < players.size(); i++){
-		//			Log.v(TAG, "Player " + i + ": " + players.get(i).getName());
-		//			FrameLayout f = new FrameLayout(this);
-		//			
-		//			View v = new View(this);
-		//			LayoutParams params = new LayoutParams(LayoutParams.MATCH_PARENT, 10);
-		//			
-		//			v.setBackgroundColor(Color.BLUE);
-		//			v.setLayoutParams(params);
-		//			
-		//			View v2 = new View(this);
-		//			LayoutParams params2 = new LayoutParams(10, LayoutParams.MATCH_PARENT);
-		//			v2.setBackgroundColor(Color.GREEN);
-		//			v2.setLayoutParams(params2);
-		//			
-		//			f.addView(v);
-		//			f.addView(v2);
-		//			
-		//			FrameLayout.LayoutParams fParams = new FrameLayout.LayoutParams(100, 100);
-		//			f.setLayoutParams(fParams);
-		//			
-		//			RelativeLayout.LayoutParams rParams = new RelativeLayout.LayoutParams(200, 100);
-		//			rParams.addRule(RelativeLayout.BELOW, R.id.textView1);
-		//			
-		//			mRelativeLayout.addView(f, rParams);
-		//		}
-
-		//startGame();
+		
 	}
 
 	/*
-	 * Shows the next edit text for the next players
+	 * Goes to the next turn or round
 	 */
 	View.OnClickListener nextTurnHandler = new View.OnClickListener() {
-		
+
 		@Override
 		public void onClick(View v) {
-			game.setCurrentRound(game.getCurrentRound() + 1);
+			numTurns++;
 			prevTurnButton.setEnabled(true);
-			if(!(game.getCurrentRound() >= game.getMarks().size())){
-				round_mark.setText(game.getMarks().get(game.getCurrentRound()).toString());
-			}
-			if(game.getCurrentRound() + 1 == game.getMarks().size())
-				nextTurnButton.setEnabled(false);
 			
-			checkMultiplers();
+			Log.v(TAG, "Mod: " + numTurns % players.size() + ", Turn: " + numTurns);
+			
+			if(numTurns % players.size() == 0 ){
+				game.setCurrentRound(game.getCurrentRound() + 1);
+				
+				Log.v(TAG, "Round: " + game.getCurrentRound());
+				
+				if(!(game.getCurrentRound() >= game.getMarks().size() * players.size())){
+					round_mark.setText(game.getMarks().get(game.getCurrentRound()).toString());
+				}
+				if(numTurns - 1 == game.getMarks().size() * players.size())
+					nextTurnButton.setEnabled(false);
+
+				checkMultiplers();
+			}else{
+				
+			}
+			updatePlayerName();
 		}
 	};
 	
+
+
 	View.OnClickListener prevTurnHandler = new View.OnClickListener() {
-		
+
+		//Goes to the previous round or player
 		@Override
 		public void onClick(View v) {
-			game.setCurrentRound(game.getCurrentRound() - 1);
-			nextTurnButton.setEnabled(true);
-			if(!(game.getCurrentRound() >= game.getMarks().size())){
-				round_mark.setText(game.getMarks().get(game.getCurrentRound()).toString());
-			}
-			if(game.getCurrentRound() == 0)
+			numTurns--;
+			if(numTurns == 0)
 				prevTurnButton.setEnabled(false);
+			nextTurnButton.setEnabled(true);
 			
-			checkMultiplers();
+			Log.v(TAG, "Mod: " + numTurns % players.size() + ", Turn: " + numTurns);
+			
+
+			if(numTurns % players.size() == players.size() - 1){
+				
+				if(numTurns != 0)
+					game.setCurrentRound(game.getCurrentRound() - 1);
+				Log.v(TAG, "Round: " + game.getCurrentRound());	
+				checkMultiplers();		
+			}
+			round_mark.setText(game.getMarks().get(game.getCurrentRound()).toString());
+			
+			updatePlayerName();
 		}
 	};
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -130,7 +133,11 @@ public class HammerGameActivity extends Activity {
 		return true;
 	}
 
-	
+	public void updatePlayerName(){
+		TextView playerName = (TextView) findViewById(R.id.textView1);
+		playerName.setText(playerNames.get(numTurns % players.size()));
+	}
+
 	public void checkMultiplers(){
 		if(game.getCurrentRound() == 7){
 			dart2_text.setText("x3");
@@ -140,7 +147,7 @@ public class HammerGameActivity extends Activity {
 			dart3_text.setText("x3");
 		}
 	}
-	
+
 	public void startGame(){
 		ArrayList<Integer> marks = game.getMarks();
 		for(int i = 0; i < marks.size(); i++){
